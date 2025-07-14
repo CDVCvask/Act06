ebe3c66ea7a47586b3c23edca844f4e39599bf73
CDVCvask <143233430+CDVCvask@users.noreply.github.com>
2025-07-14 17:24:07 -0600

    Correcciones

--- Store.py
+++ Store.py
@@ -3,8 +3,9 @@ cont = 0
 number = int(input("Ingrese cuantos productos desea ingresar: "))
 for i in range(number):
     allow = False
+    print(f"{number}")
     while allow == False:
-        Pcode = (f"P"+{cont})
+        Pcode = (f"P + {cont}")
         name = input("Ingrese el nombre del producto: ")
         category = input("Ingrese el categoria del producto: ")
         size = input("Ingrese la talla del producto: ")
@@ -18,11 +19,18 @@ for i in range(number):
                 if stock <= 0:
                     print("La cantidad ingresada no es valida")
                 else:
-                    Allow = True
+                    break
         else:
             print("La talla ingresada no es valida")
     products[Pcode] = {"name": name, "category": category, "price": price, "stock": stock}
     cont = cont + 1
+def Menu():
+    print("Venta de Ropa")
+    print("1.Mostar la lista completa de productos")
+    print("2.Buscar detalles de un producto")
+    print("3.Valor total del inventario")
+    print("4.Mostrar cuantos productos hay por categoría")
+    print("5.Salir del programa")
 allow = False
 while allow == False:
     Menu()
@@ -32,7 +40,7 @@ while allow == False:
             print("Información de todos los productos: ")
             print(" ")
             for code,value in products.items():
-
+                print(f"Nombre:{value['name']} codigo: {code}")
         case 2:
             print("Buscar")
         case 3:
@@ -44,10 +52,3 @@ while allow == False:
             break
         case _:
             print("La opción seleccionada no es valida")
-def Menu():
-    print("Venta de Ropa")
-    print("1.Mostar la lista completa de productos")
-    print("2.Buscar detalles de un producto")
-    print("3.Valor total del inventario")
-    print("4.Mostrar cuantos productos hay por categoría")
-    print("5.Salir del programa")
