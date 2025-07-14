fbba5632742abc702ab0f4d417c43977c8c172b8
CDVCvask <143233430+CDVCvask@users.noreply.github.com>
2025-07-14 16:59:04 -0600

    Ingreso

--- Store.py
+++ Store.py
@@ -8,5 +8,17 @@ for i in range(number):
         name = input("Ingrese el nombre del producto: ")
         category = input("Ingrese el categoria del producto: ")
         size = input("Ingrese la talla del producto: ")
-        price = int(input("Ingrese el precio del producto: "))
-        stock = int(input("Ingrese la cantidad del producto ingresado: "))
+        check = size.lower()
+        if check == "xs" or check == "s" or check == "m" or check == "l" or check == "xl":
+            price = int(input("Ingrese el precio del producto: "))
+            if price <= 0:
+                print("El precio ingresado no es valido")
+            else:
+                stock = int(input("Ingrese la cantidad del producto ingresado: "))
+                if stock <= 0:
+                    print("La cantidad ingresada no es valida")
+                else:
+                    Allow = True
+
+        else:
+            print("La talla ingresada no es valida")
